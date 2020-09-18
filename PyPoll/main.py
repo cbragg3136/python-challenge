e1d3190273a1ea5deb5a9981b29dbaa5f31b0150
cbragg3136 <cbragg3136@gmail.com>
2020-09-17 22:39:43 -0400

    terminal output PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,42 +3,33 @@ import csv
 
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
-def get_data(readerobj):
-    dataset = []
-    for item in readerobj:
-        dataset.append(item)
-
-    return dataset
-
-
-def voters(election_data):
-    voter_id = []
-    for item in election_data:
-        voter_id.append(item[0])
-
-    return voter_id
-
-
-def candidates(election_data):
-     candidate_votes = []
-     for item in election_data:  
-         candidate_votes.append(item[2])
-
 with open (election_data_csv, 'r') as election_data_open:
     csvreader = csv.reader(election_data_open, delimiter= ',')
 
     csv_header = next(csvreader)
-    lines = get_data(csvreader)
+  
+    candidatedict ={}
+    total_count = 0
 
-    print("Election Results\n-------------------------")  
+    for item in csvreader:
+        total_count += 1
+        if item[2] in candidatedict:
+            candidatedict[item[2]] += 1
+        else:
+            candidatedict[item[2]] = 1
 
-    resultvoter_id = voters(lines)
-    resultcandidate_votes = candidates(lines)
+  
+    print("\nElection Results\n-------------------------")  
+    print(f"Total Votes Cast: {total_count}\n-------------------------")
+    
+    candidates = []
+    values = []
+    items = candidatedict.items()
+    for item in items:
+        candidates.append(item[0]), values.append(item[1])
+        print(f'{item[0]}: {round(item[1]*100/total_count,1)}% ({item[1]})')
 
-    total_voters = len(resultvoter_id)
-    print(f"Total Votes Cast: {total_voters}")
-
-    candidate_list = (resultcandidate_votes)
-
+    print("-------------------------")
+    print(f"Winner: {candidates[values.index(max(values))]}")
 
 
